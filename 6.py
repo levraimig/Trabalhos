095086495c68d2e74effa5696a4a7c007efeebff
Miguel Paniz <migpaniz@gmail.com>
2022-05-25 09:53:06 -0300

    tentantado fazer a 6

--- 6.py
+++ 6.py
@@ -1,57 +1,27 @@
-cont = 0
-cont2 = 0
-cont3 = 0
-cont4 = 0
-cont5 = 0
-cont6 = 0
-cont7 = 0
-cont8 = 0
-cont9 = 0
-cont10 = 0
+cont_alunos = 0
 media_alunos = []
 
 
-while cont < 4:
-    aluno1 = float(input('Digite a nota do aluno 1:'))
-    cont = cont + 1
 
-while cont2 <4:
-    aluno2 = float(input('Digite a nota do aluno 2:'))
-    cont2 = cont2 + 1
+while cont_alunos <= 1:
+    cont = 0
+    lista_notas = []
 
-while cont3 <4:
-    aluno3 = float(input('Digite a nota do aluno 3:'))
-    cont3 = cont3 + 1
+    while cont < 4:
+        aluno1 = float(input('Digite a nota do aluno %d:' % (cont_alunos + 1)))
+        cont = cont + 1
+        lista_notas.append(aluno1)
 
-while cont4 <4:
-    aluno4 = float(input('Digite a nota do aluno 4:'))
-    cont4 = cont4 + 1
+    # computar a média depois q tem as 4 notas
+    # adicionar a média a lista de médias
+    media_alunos = sum(lista_notas) / len(lista_notas)
+    cont_alunos = cont_alunos + 1
 
-while cont5 <4:
-    aluno5 = float(input('Digite a nota do aluno 5:'))
-    cont5 = cont5 + 1
+print(media_alunos)
 
-while cont6 <4:
-    aluno6 = float(input('Digite a nota do aluno 6:'))
-    cont6 = cont6 + 1
 
-while cont7 <4:
-    aluno7 = float(input('Digite a nota do aluno 7:'))
-    cont7 = cont7 + 1
 
-while cont8 <4:
-    aluno8 = float(input('Digite a nota do aluno 8:'))
-    cont8= cont8 + 1
 
-while cont9 <4:
-    aluno9 = float(input('Digite a nota do aluno 9:'))
-    cont9 = cont9 + 1
-
-while cont10 <4:
-    aluno10 = float(input('Digite a nota do aluno 10'))
-    cont10= cont10+ 1
-
-print(aluno1+1)
 
 
 
